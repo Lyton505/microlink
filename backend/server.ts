d30d986929aaa67221a3676edb6e600b32bbb5c4
Lyton Mhlanga <lytonm@lyton.dev>
2024-10-06 22:05:48 -0500

    chore: configure prettier to work with ts and gts on a new ts version

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,5 @@
-import express from 'express';
-import {Request, Response} from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World');
-});
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+console.log("Hello World");
